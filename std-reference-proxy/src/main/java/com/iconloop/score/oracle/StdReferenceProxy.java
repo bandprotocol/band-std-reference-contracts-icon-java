d54dd40877a3bfe2ba16ef1c0b611a6141c90fda
warittornc <69615455+warittornc@users.noreply.github.com>
2022-12-19 10:55:35 +0700

    changed params to reflect older contract for compatability

--- std-reference-proxy/src/main/java/com/iconloop/score/oracle/StdReferenceProxy.java
+++ std-reference-proxy/src/main/java/com/iconloop/score/oracle/StdReferenceProxy.java
@@ -40,12 +40,12 @@ public class StdReferenceProxy {
     }
 
     @External(readonly = true)
-    public Map<String, BigInteger> get_reference_data(String base, String quote) {
+    public Map<String, BigInteger> get_reference_data(String _base, String _quote) {
         return (Map<String, BigInteger>) Context.call(Map.class, this.ref, "getReferenceData", base, quote);
     }
 
     @External(readonly = true)
-    public List<Map<String, BigInteger>> get_reference_data_bulk(String[] bases, String[] quotes) {
+    public List<Map<String, BigInteger>> get_reference_data_bulk(String[] _bases, String[] _quotes) {
         return (List<Map<String, BigInteger>>) Context.call(List.class, this.ref, "getReferenceDataBulk", bases,
                 quotes);
     }
